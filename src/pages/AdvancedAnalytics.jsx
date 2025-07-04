7360f87203619adfdbf8ae6fdc3b1104d59ce68d
1tietennis <tietennis@gmail.com>
2025-07-04 17:03:31 -0400

    Added files via Greta

---
+++ src/pages/AdvancedAnalytics.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AdvancedAnalyticsDashboard from '../components/analytics/AdvancedAnalyticsDashboard'
+
+const AdvancedAnalytics = () => {
+  return <AdvancedAnalyticsDashboard />
+}
+
+export default AdvancedAnalytics
---
+++ src/pages/CheckoutOptimization.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CheckoutOptimizer from '../components/checkout/CheckoutOptimizer'
+
+const CheckoutOptimization = () => {
+  return <CheckoutOptimizer />
+}
+
+export default CheckoutOptimization
